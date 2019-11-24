099c243a7960926332d90c447a91bc79a23bbe21
La La <36699428+kiaoraquok@users.noreply.github.com>
2019-11-24 18:59:04 +0200

    Add newline at end of file in Stepdefs.java

--- src/test/java/main/Stepdefs.java
+++ src/test/java/main/Stepdefs.java
@@ -10,25 +10,25 @@ import static org.junit.Assert.*;
 
 public class Stepdefs {
     List<String> inputLines;
-    
+
     @Before
     public void setup(){
-        inputLines = new ArrayList<>();      
+        inputLines = new ArrayList<>();
     }
-    
+
     @Given("command Lisaa Otsikolla is selected")
     public void commandLisaaOtsikollaSelected() {
-        
+
     }
 
     @When("title {string} is entered")
     public void titleIsEntered(String title) {
-        
+
     }
-    
+
     @Then("system will respond with {string}")
     public void systemWillRespondWith(String expectedOutput) {
-        
+
     }
 
 }
