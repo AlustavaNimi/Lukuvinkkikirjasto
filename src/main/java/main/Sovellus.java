7f4d1d02105e2174dd3725d20a1991220b86e4e7
La La <36699428+kiaoraquok@users.noreply.github.com>
2019-11-26 23:30:43 +0200

    Comment out JavaFX imports and un-JavaFXify Sovellus-class to test CircleCI issues

--- src/main/java/main/Sovellus.java
+++ src/main/java/main/Sovellus.java
@@ -6,22 +6,22 @@ import UI.TekstiKayttoliittyma;
 import database.KirjaDao;
 import database.Tietokanta;
 import java.util.Scanner;
-import javafx.application.Application;
-import static javafx.application.Application.launch;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+//import javafx.application.Application;
+//import static javafx.application.Application.launch;
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+//import javafx.scene.layout.StackPane;
+//import javafx.stage.Stage;
 import javax.swing.SwingUtilities;
 
-public class Sovellus extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        System.out.println("test");
-    }
+public class Sovellus {
+//
+//    @Override
+//    public void start(Stage primaryStage) {
+//        System.out.println("test");
+//    }
 
     public static void main(String[] args) {
         boolean graafinen = false;
