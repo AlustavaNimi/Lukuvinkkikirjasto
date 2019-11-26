a4787769c78a57ea2691b4b9c334584907d80bfc
Olli Matilainen <olli.e.matilainen@gmail.com>
2019-11-26 15:50:58 +0200

    javafx initialized

--- src/main/java/main/Sovellus.java
+++ src/main/java/main/Sovellus.java
@@ -1,14 +1,35 @@
 
 import IO.KonsoliIO;
+import UI.GraafinenKayttoliittyma;
 import UI.Kayttoliittyma;
 import UI.TekstiKayttoliittyma;
 import java.util.Scanner;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 
-public class Sovellus {
+public class Sovellus extends Application{
     
+    @Override
+    public void start(Stage primaryStage) {
+        new GraafinenKayttoliittyma(primaryStage);
+    }
+
     public static void main(String[] args) {
-        Kayttoliittyma kayttoliittyma = new TekstiKayttoliittyma(new KonsoliIO());
-        kayttoliittyma.run();
+        boolean graafinen = true;
+        //cubbli-linuxilla anna komentorivillä komento
+        //export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
+        if(graafinen) {
+            launch(args);
+        } else {
+            Kayttoliittyma kayttoliittyma = new TekstiKayttoliittyma(new KonsoliIO());
+            kayttoliittyma.run();
+        }
     }
 }
