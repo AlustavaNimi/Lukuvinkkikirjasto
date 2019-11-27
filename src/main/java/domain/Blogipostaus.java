31edbbfbebda5f298603e039332fcf9c4b0042ab
La La <36699428+kiaoraquok@users.noreply.github.com>
2019-11-27 02:44:02 +0200

    Rename Blogi-class to Blogipostaus

--- src/main/java/domain/Blogipostaus.java
+++ src/main/java/domain/Blogipostaus.java
@@ -1,9 +1,9 @@
 package domain;
 
-public class Blogi extends Lukuvinkki {
+public class Blogipostaus extends Lukuvinkki {
     private String url;
 
-    public Blogi(String url, String otsikko, String kuvaus, String kurssi, int julkaisuVuosi) {
+    public Blogipostaus(String url, String otsikko, String kuvaus, String kurssi, int julkaisuVuosi) {
         super(otsikko, kuvaus, kurssi, julkaisuVuosi);
         this.url = url;
     }
@@ -20,7 +20,7 @@ public class Blogi extends Lukuvinkki {
     public String toString() {
         String tuloste = super.toString();
         tuloste += "\nURL: " + this.getUrl();
-        tuloste += "\nTyyppi: Blogi";
+        tuloste += "\nTyyppi: Blogipostaus";
         return tuloste;
     }    
     
