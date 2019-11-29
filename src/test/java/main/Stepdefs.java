48ce19e4f5d66b8fc71c0b0e28371d33d4fbfc76
anitapenttila <anitakpenttila@gmail.com>
2019-11-29 18:46:48 +0200

    Edit Stepdefs.java and new_book_suggestion.feature

--- src/test/java/main/Stepdefs.java
+++ src/test/java/main/Stepdefs.java
@@ -32,7 +32,7 @@ public class Stepdefs {
     @When("title {string} is entered")
     public void titleIsEntered(String title) {
         inputLines.add(title);
-        inputLines.add("lopeta");
+        inputLines.add("lisaa otsikolla");
         
         io = new IOStub();
         io.setInputs(inputLines);
@@ -43,6 +43,24 @@ public class Stepdefs {
     @Then("system will respond with {string}")
     public void systemWillRespondWith(String expectedOutput) {
         assertTrue(io.getOutputs().contains(expectedOutput));
+    }
+    
+    @Given("command lisaa vinkki is selected")
+    public void commandLisaaVinkkiIsSelected() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("title {string}, author {string}, isbn {string}, description {string}, publication date {string}, course {string} and url {string} are entered")
+    public void titleAuthorIsbnDescriptionPublicationDateCourseAndUrlAreEntered(String string, String string2, String string3, String string4, String string5, String string6, String string7) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("title {string} and publication date {string} is entered")
+    public void titleAndPublicationDateIsEntered(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
     }
 
 }
--- src/test/resources/main/new_book_suggestion.feature
+++ src/test/resources/main/new_book_suggestion.feature
@@ -1,6 +1,21 @@
 Feature: A new book suggestion can be created
 
     Scenario: creation is successful with valid title
-        Given command lisaa otsikolla is selected
+        Given command lisaa vinkki is selected
         When title "Clean Code: A Handbook of Agile Software Craftsmanship" is entered
         Then system will respond with "Uusi lukuvinkki lisätty"
+
+    Scenario: creation is successful with valid title, author, isbn, description, publication date, course and url
+        Given command lisaa vinkki is selected
+        When title "Tirakirja", author "Antti Laaksonen", isbn "-", description "jee tira", publication date "2019", course "Tietorakenteet ja algoritmit" and url "https://www.cs.helsinki.fi/u/ahslaaks/tirakirja/" are entered
+        Then system will respond with "Uusi lukuvinkki lisätty"
+    
+    Scenario: creation fails with invalid publication date
+        Given command lisaa vinkki is selected
+        When  title "Tirakirja" and publication date "today" is entered
+        Then  system will respond with "Vuosiluvun on oltava luku"
+
+    Scenario: creation fails without valid title
+        Given command lisaa vinkki is selected
+        When  title "" is entered
+        Then  system will respond with "Lukuvinkille on annettava otsikko"
