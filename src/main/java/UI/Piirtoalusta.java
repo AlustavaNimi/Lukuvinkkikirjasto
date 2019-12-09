982cfbe93a080935eb068c036ea7c7e376fc7e5c
Olli Matilainen <olli.e.matilainen@gmail.com>
2019-12-10 00:53:44 +0200

    Fix layout

--- src/main/java/UI/Piirtoalusta.java
+++ src/main/java/UI/Piirtoalusta.java
@@ -3,6 +3,7 @@ package UI;
 import domain.Blogipostaus;
 import domain.Kirja;
 import domain.Lukuvinkki;
+import java.awt.FlowLayout;
 import java.awt.Point;
 import java.util.ArrayList;
 import javax.swing.*;
@@ -55,7 +56,7 @@ public class Piirtoalusta extends JPanel {
         blogiCheckBox = new JCheckBox("Blogit");
         kirjaCheckBox.setSelected(true);
         blogiCheckBox.setSelected(true);
-        this.add(scroll);       
+        this.add(scroll);
         if (muokkaus) {
             poistaNappi = new JButton("Poista");
             muokkausNappi = new JButton("Muokkaa");
@@ -67,15 +68,23 @@ public class Piirtoalusta extends JPanel {
                 this.add(linkkiNappi);
             }
         } else {
-            this.add(new JLabel("Valitse numerolla"));
-            this.add(input);
-            this.add(kirjaCheckBox);
-            this.add(blogiCheckBox);
-            this.add(new JLabel("Hae hakusanalla"));
-            this.add(hakuKentta);
-            this.add(lisaaNappi);
-            this.add(selaaNappi);
-            this.add(lopetaNappi);
+            JPanel p1 = new JPanel(new FlowLayout());
+            p1.add(new JLabel("Valitse numerolla"));
+            p1.add(input);
+            JPanel p2 = new JPanel(new FlowLayout());
+            p2.add(kirjaCheckBox);
+            p2.add(blogiCheckBox);
+            JPanel p3 = new JPanel(new FlowLayout());
+            p3.add(new JLabel("Hae hakusanalla"));
+            p3.add(hakuKentta);
+            JPanel p4 = new JPanel(new FlowLayout());
+            p4.add(lisaaNappi);
+            p4.add(selaaNappi);
+            p4.add(lopetaNappi);
+            this.add(p1);
+            this.add(p2);
+            this.add(p3);
+            this.add(p4);
         }
         this.add(alkuunNappi);
         frame.getContentPane().add(this);
@@ -110,30 +119,30 @@ public class Piirtoalusta extends JPanel {
     public JFrame lukuvinkinMuokkaus(JFrame frame, Lukuvinkki vinkki) {
         vinkinTiedot = new ArrayList<>();
         JTextField isbn = new JTextField();
-        JTextField tyyppi = new JTextField();        
+        JTextField tyyppi = new JTextField();
         JTextField otsikko = new JTextField(vinkki.getOtsikko(), 20);
-        JTextField kirjoittaja = new JTextField(vinkki.getKirjoittaja(), 18);       
+        JTextField kirjoittaja = new JTextField(vinkki.getKirjoittaja(), 18);
         JTextField kuvaus = new JTextField(vinkki.getKuvaus(), 20);
         String vuosi = String.valueOf(vinkki.getJulkaisuVuosi());
         JTextField vuosiluku = new JTextField(vuosi, 18);
         JTextField kurssi = new JTextField(vinkki.getKurssi(), 20);
         if (vinkki instanceof Kirja) {
             Kirja kirja = (Kirja) vinkki;
-            isbn = new JTextField(kirja.getISBN(),20);
+            isbn = new JTextField(kirja.getISBN(), 20);
             tyyppi = new JTextField("kirja", 20);
-        }   else if (vinkki instanceof Blogipostaus) {
+        } else if (vinkki instanceof Blogipostaus) {
             Blogipostaus postaus = (Blogipostaus) vinkki;
-            isbn = new JTextField(postaus.getUrl(),20);
-            tyyppi = new JTextField("blogi", 20); 
+            isbn = new JTextField(postaus.getUrl(), 20);
+            tyyppi = new JTextField("blogi", 20);
         }
         vinkinTiedot.add(otsikko);
         vinkinTiedot.add(kirjoittaja);
         vinkinTiedot.add(kuvaus);
         vinkinTiedot.add(vuosiluku);
-        vinkinTiedot.add(kurssi); 
+        vinkinTiedot.add(kurssi);
         vinkinTiedot.add(isbn);
         vinkinTiedot.add(tyyppi);
-        
+
         alkuunNappi = new JButton("Alkuun");
         alkuunNappi.addActionListener(kuuntelija);
 
@@ -141,20 +150,34 @@ public class Piirtoalusta extends JPanel {
         tallennaNappi.addActionListener(kuuntelija);
 
         alkuunNappi.setLocation(new Point(195, 313));
-        this.add(new JLabel("otsikko"));
-        this.add(otsikko);
-        this.add(new JLabel("kirjoittaja"));
-        this.add(kirjoittaja);
-        this.add(new JLabel("isbn/url"));
-        this.add(isbn);
-        this.add(new JLabel("kuvaus"));
-        this.add(kuvaus);
-        this.add(new JLabel("vuosiluku"));
-        this.add(vuosiluku);
-        this.add(new JLabel("kurssi"));
-        this.add(kurssi);
-        this.add(new JLabel("tyyppi"));
-        this.add(tyyppi);
+        JPanel p1 = new JPanel(new FlowLayout());
+        p1.add(new JLabel("Otsikko"));
+        p1.add(otsikko);
+        JPanel p2 = new JPanel(new FlowLayout());
+        p2.add(new JLabel("Kirjoittaja"));
+        p2.add(kirjoittaja);
+        JPanel p3 = new JPanel(new FlowLayout());
+        p3.add(new JLabel("ISBN/URL"));
+        p3.add(isbn);
+        JPanel p4 = new JPanel(new FlowLayout());
+        p4.add(new JLabel("Kuvaus"));
+        p4.add(kuvaus);
+        JPanel p5 = new JPanel(new FlowLayout());
+        p5.add(new JLabel("Vuosiluku"));
+        p5.add(vuosiluku);
+        JPanel p6 = new JPanel(new FlowLayout());
+        p6.add(new JLabel("Kurssi"));
+        p6.add(kurssi);
+        JPanel p7 = new JPanel(new FlowLayout());
+        p7.add(new JLabel("Tyyppi"));
+        p7.add(tyyppi);
+        this.add(p1);
+        this.add(p2);
+        this.add(p3);
+        this.add(p4);
+        this.add(p5);
+        this.add(p6);
+        this.add(p7);
 
         this.add(tallennaNappi);
         this.add(alkuunNappi);
@@ -196,20 +219,35 @@ public class Piirtoalusta extends JPanel {
         tallennaNappi.addActionListener(kuuntelija);
 
         alkuunNappi.setLocation(new Point(195, 313));
-        this.add(new JLabel("otsikko"));
-        this.add(otsikko);
-        this.add(new JLabel("kirjoittaja"));
-        this.add(kirjoittaja);
-        this.add(new JLabel("isbn/url"));
-        this.add(isbn);
-        this.add(new JLabel("kuvaus"));
-        this.add(kuvaus);
-        this.add(new JLabel("vuosiluku"));
-        this.add(vuosiluku);
-        this.add(new JLabel("kurssi"));
-        this.add(kurssi);
-        this.add(new JLabel("tyyppi"));
-        this.add(tyyppi);
+
+        JPanel p1 = new JPanel(new FlowLayout());
+        p1.add(new JLabel("Otsikko"));
+        p1.add(otsikko);
+        JPanel p2 = new JPanel(new FlowLayout());
+        p2.add(new JLabel("Kirjoittaja"));
+        p2.add(kirjoittaja);
+        JPanel p3 = new JPanel(new FlowLayout());
+        p3.add(new JLabel("ISBN/URL"));
+        p3.add(isbn);
+        JPanel p4 = new JPanel(new FlowLayout());
+        p4.add(new JLabel("Kuvaus"));
+        p4.add(kuvaus);
+        JPanel p5 = new JPanel(new FlowLayout());
+        p5.add(new JLabel("Vuosiluku"));
+        p5.add(vuosiluku);
+        JPanel p6 = new JPanel(new FlowLayout());
+        p6.add(new JLabel("Kurssi"));
+        p6.add(kurssi);
+        JPanel p7 = new JPanel(new FlowLayout());
+        p7.add(new JLabel("Tyyppi"));
+        p7.add(tyyppi);
+        this.add(p1);
+        this.add(p2);
+        this.add(p3);
+        this.add(p4);
+        this.add(p5);
+        this.add(p6);
+        this.add(p7);
 
         this.add(tallennaNappi);
         this.add(alkuunNappi);
@@ -223,23 +261,23 @@ public class Piirtoalusta extends JPanel {
     public JButton getTallennaNappi() {
         return tallennaNappi;
     }
-    
+
     public JButton getPoistaNappi() {
         return poistaNappi;
     }
-    
+
     public JButton getMuokkausNappi() {
         return muokkausNappi;
     }
-    
+
     public JButton getSelaaNappi() {
         return this.selaaNappi;
     }
-    
+
     public JButton getLisaaNappi() {
         return this.lisaaNappi;
     }
-    
+
     public JButton getLopetaNappi() {
         return this.lopetaNappi;
     }
@@ -251,16 +289,16 @@ public class Piirtoalusta extends JPanel {
     public JButton getLinkkiNappi() {
         return this.linkkiNappi;
     }
-    
+
     public JTextField getHakuKentta() {
         return this.hakuKentta;
     }
-    
+
     public JCheckBox getKirjaCheckBox() {
         return this.kirjaCheckBox;
     }
-    
-     public JCheckBox getBlogiCheckBox() {
+
+    public JCheckBox getBlogiCheckBox() {
         return this.blogiCheckBox;
     }
 }
