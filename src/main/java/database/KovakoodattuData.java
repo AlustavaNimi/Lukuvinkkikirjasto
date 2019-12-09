c592a84a6a9b98c1c8824700465aea40123a479a
La La <36699428+kiaoraquok@users.noreply.github.com>
2019-12-09 23:19:45 +0200

    Refaktoroi harjoitustehtävänä viikolle 6

--- src/main/java/database/KovakoodattuData.java
+++ src/main/java/database/KovakoodattuData.java
@@ -4,20 +4,20 @@ import domain.Blogipostaus;
 import domain.Kirja;
 
 public class KovakoodattuData {
-    
+
     //private String tietokantaOsoite;
     private Tietokanta tk;
     private Kirja kirja1;
-    private Blogipostaus blogi1;
-    
+    private Blogipostaus blogipostaus1;
+
     public KovakoodattuData() {
         this.kirja1 = new Kirja(1, "Tirakirja", "A. Laaksonen", "9788373191723", "Ei hajota yhtään", 2019, "Tietorakenteet ja algoritmit");
-        this.blogi1 = new Blogipostaus(2, "https://blogit.kaleva.fi/taidon-aani/mita-on-tietojenkasittely",
+        this.blogipostaus1 = new Blogipostaus(2, "https://blogit.kaleva.fi/taidon-aani/mita-on-tietojenkasittely",
                 "Mitä on tietojenkäsittely", "Lyhyt esittely alaan", "JTKT", "Taidon Ääni", 2019);
     }
-    
+
     public void lisaaKovakoodattuData(Tietokanta tk) {
         tk.lisaaKirja(kirja1);
-        tk.lisaaBlogi(blogi1);
+        tk.lisaaBlogi(blogipostaus1);
     }
 }
